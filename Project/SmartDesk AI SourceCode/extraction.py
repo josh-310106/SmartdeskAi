452f2ebbbd4f82dc130a24cbd3e5adbeff297d3b
hari2111y <hariragav1003@gmail.com>
2026-06-10 10:56:22 +0530

    Delete Project/SmartDesk AI SourceCode directory

--- Project/SmartDesk AI SourceCode/extraction.py
+++
@@ -1,4 +0,0 @@
-# Wrapper to redirect extraction calls to the main ticket_extractor module
-from ticket_extractor import extract_ticket_info, get_mock_extraction
-
-__all__ = ["extract_ticket_info", "get_mock_extraction"]
